87af0aed8bd2810a422f0f68e79cebed60aa3ed4
Triceratroll <adrianmf06@gmail.com>
2022-06-22 14:41:06 +0200

    test bundled app

---
+++ app.e4647441.js
@@ -0,0 +1,4 @@
+(function(){ try {var elementStyle = document.createElement('style'); elementStyle.appendChild(document.createTextNode("#app{font-family:Avenir,Helvetica,Arial,sans-serif;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;text-align:center;color:#2c3e50;margin-top:60px}")); document.head.appendChild(elementStyle);} catch(e) {console.error('vite-plugin-css-injected-by-js', e);} })();document.querySelector("#app").innerHTML=`
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
